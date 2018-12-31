e66cbda97663ae9f303cd3c4a9e750b13f05c975
Anton Fedoseev <aranoy15@gmail.com>
2018-12-31 12:22:45 +0300

    Initial Commit

---
+++ cmake/CompilerFlags.cmake
@@ -0,0 +1,34 @@
+# specify the cross compiler
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+
+set(CMAKE_ASM-ATT_COMPILER arm-none-eabi-gcc)
+enable_language(ASM-ATT)
+set(CMAKE_ASM-ATT_COMPILER arm-none-eabi-gcc)
+
+# search for programs in the build host directories
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# for libraries and headers in the target directories
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS True)
+
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+#set(COMMON_FLAGS "-W -Wall -fdiagnostics-color=always")
+
+if (DBG)
+    set(COMMON_FLAGS "-g ${COMMON_FLAGS}")
+endif ()
+
+add_definitions(-DSTM32F417xx)
+add_definitions(-DUSE_HAL_DRIVER)
+
+set(OPT_FLAGS "-Os -ffunction-sections -fdata-sections")
+set(PLATFORM_FLAGS "-mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb")
+set(CMAKE_CXX_FLAGS "-Weffc++ ${CMAKE_CXX_FLAGS} ${COMMON_FLAGS} ${OPT_FLAGS} ${PLATFORM_FLAGS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_FLAGS} ${OPT_FLAGS} ${PLATFORM_FLAGS}")
+set(CMAKE_ASM-ATT_FLAGS "${CMAKE_ASM-ATT_FLAGS} -c ${COMMON_FLAGS} ${PLATFORM_FLAGS}")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} ${COMMON_FLAGS} ${PLATFORM_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -specs=nosys.specs -lnosys -lc -lm -Xlinker --gc-sections")
---
+++ cmake/Tools.cmake
@@ -0,0 +1,28 @@
+function (add_trace NAME)
+	add_custom_target(${NAME}.trace
+		arm-none-eabi-gdb --batch --command=${CMAKE_SOURCE_DIR}/debug/trace_gdb.py --args ${CMAKE_CURRENT_BINARY_DIR}/${NAME}
+		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+		USES_TERMINAL
+		)
+endfunction ()
+
+function (flash NAME)
+	add_custom_target(flash
+		st-flash write ${CMAKE_BINARY_DIR}/${NAME}.bin 0x8000000
+		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+		USES_TERMINAL
+		)
+endfunction ()
+
+function(size NAME)
+	add_custom_target(size ALL
+		arm-none-eabi-size ${CMAKE_BINARY_DIR}/${NAME}/${NAME}
+		DEPENDS ${NAME}
+	)	
+endfunction(size NAME)
+
+
+add_custom_target(openocd
+openocd -f /usr/share/openocd/scripts/interface/stlink-v2.cfg -f /usr/share/openocd/scripts/target/stm32f1x.cfg
+	USES_TERMINAL
+)
