6d2762fb08746dc2c08e76d8373b72762f78aac0
Anton Fedoseev <aranoy15@gmail.com>
2019-02-06 15:07:07 +0300

    Внес изменения в основной файл.

--- logic/main/app.c
+++ logic/main/app.c
@@ -3,7 +3,10 @@
 #include <gpio.h>
 #include <rcc.h>
 #include <usbdevice.h>
+#include <watchdog.h>
 #include <cmsis_os.h>
+#include <i2c.h>
+#include <string.h>
 
 void* malloc(size_t size)
 {
@@ -24,6 +27,7 @@ void InitAll()
 	HAL_Init();
 	RccInit();
 	UsbDeviceInit();
+	I2cInit();
 }
 
 void blinkTask(void *data)
@@ -35,16 +39,32 @@ void blinkTask(void *data)
 		osDelay(250);
 		GpioOff(CSP_GPIO_PORT_NBR_C, GPIO_PIN_13);
 		osDelay(250);
+		WatchdogReload();
 	}
 }
 
 void sendTask(void *data)
 {
-	uint8_t message[] = "HI, ANTON!\n";
+	char info[] = "Scanning I2C bus...\r\n";
 	
 	for(;;) {
-		CDC_Transmit_FS(message, strlen((const char*)message));
-		osDelay(2000);
+		CDC_Transmit_FS(info, strlen(info));
+
+		HAL_StatusTypeDef res;
+		for (uint16_t i = 0; i < 128; i++) {
+			res = HAL_I2C_IsDeviceReady(&hi2c1, i << 1, 1, 10);
+			if (res == HAL_OK) {
+				//char msg[64];
+				//sprintf(msg, "0x%02X", i);
+				CDC_Transmit_FS("OK", strlen("OK"));
+			} else {
+				CDC_Transmit_FS((uint8_t*)".", 1);
+			}
+		}
+
+		CDC_Transmit_FS((uint8_t*)"\r\n", 2);
+
+		osDelay(5000);
 	}
 }
 
@@ -55,6 +75,10 @@ int main()
 
 	osThreadDef(blink, blinkTask, osPriorityNormal, 0, 1024 / sizeof(size_t));
 	osThreadCreate(osThread(blink), NULL);
+
+	WatchdogInit();
+	WatchdogStart();
+	WatchdogReload();
 
 	osKernelStart();
 }
