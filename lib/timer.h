585f0fd8eb7e780e1abb9e048f38a71dc0890537
Anton Fedoseev <aranoy15@gmail.com>
2019-05-31 13:58:30 +0300

    Добавил класс таймера

---
+++ lib/timer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <time.h>
+
+class Timer
+{
+    uint32_t m_timeout;
+    uint32_t m_startTick;
+
+public:
+	Timer() : m_timeout(0), m_startTick(0) {}
+
+	void start(uint32_t timeout) 
+    {
+        m_timeout = timeout; 
+        m_startTick = Time::getTicks();
+    }
+
+    bool elapsed() { return ((Time::getTicks() - m_startTick) > m_timeout); }
+};
