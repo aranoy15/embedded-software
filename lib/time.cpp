e7f62137ac1a9f71f197865da8c4234eebe581ed
Anton Fedoseev <aranoy15@gmail.com>
2019-06-10 14:13:44 +0300

    Убрал поддержку FreeRTOS из драйвера time

--- lib/time.cpp
+++ lib/time.cpp
@@ -1,20 +1,24 @@
 #include <time.h>
-#include <cmsis_os.h>
+//#include <cmsis_os.h>
 
 void Time::sleep(const Time& t)
 {
 	if (t.m_time <= 0) return;
 
+	/*
 	if (osKernelRunning())
 		osDelay(t.m_time);
 	else
-		HAL_Delay(t.m_time);
+	*/
+	HAL_Delay(t.m_time);
 }
 
 uint32_t Time::getTicks()
 {
+	/*
 	if(osKernelRunning())
 		return osKernelSysTick();
 	else
-		return HAL_GetTick();
+	*/
+	return HAL_GetTick();
 }
