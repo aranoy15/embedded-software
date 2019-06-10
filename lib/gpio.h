499dc1dc9f1105f5c98011473c7f04d2b626fafc
Anton Fedoseev <aranoy15@gmail.com>
2019-06-10 14:05:00 +0300

    Убрал лишние строки в gpio

--- lib/gpio.h
+++ lib/gpio.h
@@ -100,30 +100,3 @@ struct GPIO
 	}
 };
 }  // namespace gpio
-
-
-
-/*
-#pragma once
-
-#include <stdint.h>
-#include <stm32f1xx_hal.h>
-
-typedef uint16_t CSP_DEV_NBR;
-typedef uint32_t CSP_GPIO_MSK;
-
-#define CSP_GPIO_PORT_NBR_A (CSP_DEV_NBR)(0u)
-#define CSP_GPIO_PORT_NBR_B (CSP_DEV_NBR)(1u)
-#define CSP_GPIO_PORT_NBR_C (CSP_DEV_NBR)(2u)
-#define CSP_GPIO_PORT_NBR_D (CSP_DEV_NBR)(3u)
-#define CSP_GPIO_PORT_NBR_E (CSP_DEV_NBR)(4u)
-
-void GpioSetup(CSP_DEV_NBR port, CSP_GPIO_MSK pins, uint32_t mode, uint16_t speed,
-                 uint32_t pull);
-GPIO_TypeDef *GpioGetPointer(CSP_DEV_NBR portNum);
-
-void GpioOn(CSP_DEV_NBR port, CSP_GPIO_MSK pin);
-void GpioOff(CSP_DEV_NBR port, CSP_GPIO_MSK pin);
-uint8_t GpioState(CSP_DEV_NBR port, CSP_GPIO_MSK pin);
-void GpioRemove(CSP_DEV_NBR port, CSP_GPIO_MSK pin);
-*/
