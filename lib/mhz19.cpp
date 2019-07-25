c225219d638e51bec7c8f0807bebcaff57fd9bc6
Anton Fedoseev <aranoy15@gmail.com>
2019-07-25 09:18:47 +0300

    Добавил полную поддержку датчика co2 mhz19b

--- lib/mhz19.cpp
+++ lib/mhz19.cpp
@@ -1,33 +1,66 @@
 #include <mhz19.h>
 
-Mhz19::Mhz19()
+Mhz19Data::Mhz19Data() : co2(0), temperature(0), status(0) {}
+
+Mhz19::Mhz19() : buffer()
 {
-    //getUart().init(32, 9600);
 }
 
+void Mhz19::init()
+{
+    getUart().init(32, 9600);
+}
 
 uint16_t Mhz19::getCO2()
 {
     uint16_t result = 0;
+    command(0x86, static_cast<uint8_t>(0));
 
-    uint8_t buffer[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    buffer[8] = calcCrc(buffer);
-    getUart().send(buffer, 9);
-
-	if (getUart().readCount(buffer, 9, 1000)) {
-		result = (buffer[2] << 8) | buffer[3];
-	}
-
+    if (readResponse()) {
+        result = (buffer[2] << 8) | buffer[3];
+    }
 
 	return result;
 }
 
-uint8_t Mhz19::calcCrc(uint8_t data[])
+void Mhz19::command(uint8_t command, uint8_t high, uint8_t low)
+{
+    buffer[0] = 0xFF;
+    buffer[1] = 0x01;
+    buffer[2] = command;
+    buffer[3] = high;
+    buffer[4] = low;
+    buffer[8] = calcCrc();
+
+    //getLog().dump(buffer, bufferSize, "Command data: ");
+    getUart().send(buffer, bufferSize);
+}
+
+bool Mhz19::readResponse()
+{
+    bool result = getUart().readCount(buffer, bufferSize, 100);
+    //getLog().dump(buffer, bufferSize, "Response data: ");
+
+    if (result)
+        result = (buffer[8] == calcCrc());
+
+    return result;
+}
+
+
+
+void Mhz19::setAutoCalibration(bool state)
+{
+    command(OnOffAutoCalibration, state ? 0xA0 : 0x00);
+}
+
+
+uint8_t Mhz19::calcCrc()
 {
     uint8_t crc = 0;
 
-    for (uint8_t i = 1; i < 8; i++) {
-        crc += data[i];
+    for (uint8_t i = 1; i < bufferSize - 1; i++) {
+        crc += buffer[i];
     }
 
     crc = 255 - crc;
--- lib/mhz19.h
+++ lib/mhz19.h
@@ -3,6 +3,15 @@
 #include <uart.h>
 #include <vector>
 
+struct Mhz19Data
+{
+	int co2;
+	int temperature;
+	int status;
+
+	Mhz19Data();
+};
+
 class Mhz19
 {
 private:
@@ -11,11 +20,33 @@ private:
 		return *Uart<uart::UartPort::usart2>::instance();
 	}
 
+	Uart<uart::UartPort::usart1>& getLog()
+	{
+		return *Uart<uart::UartPort::usart1>::instance();
+	}
+
+	static const uint8_t bufferSize = 9;
+	uint8_t buffer[bufferSize];
+
+	enum Commands
+	{
+		ReadCO2Concetration = 0x86,
+		CalibrateZeroPoint = 0x87,
+		CalibrateSpanPoint = 0x88,
+		OnOffAutoCalibration = 0x79,
+		DetectionRangeSetting = 0x99
+	};
+
 public:
     Mhz19();
 
+	void init();
     uint16_t getCO2();
+	void setAutoCalibration(bool);
 
 private:
-    uint8_t calcCrc(uint8_t data[]);
+	void command(uint8_t, uint8_t = 0, uint8_t = 0);
+	bool readResponse();
+
+    uint8_t calcCrc();
 };
